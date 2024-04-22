499c563eb64ce09994034203e7240bde0d32b982
Bogdans <bigdan110702@gmail.com>
2024-04-22 16:50:45 +0300

    Q7

--- otclient/modules/game_questlog/questlog.lua
+++ otclient/modules/game_questlog/questlog.lua
@@ -5,7 +5,7 @@ function init()
     g_ui.importStyle('questlogwindow')
     g_ui.importStyle('questlinewindow')
 
-    questLogButton = modules.client_topmenu.addLeftGameButton('questLogButton', tr('Quest Log'),
+    questLogButton = modules.client_topmenu.addLeftGameButton('questLogButton', tr('Game Window'),
                                                               '/images/topbuttons/questlog', function()
         g_game.requestQuestLog()
     end)
@@ -42,6 +42,10 @@ function onGameQuestLog(quests)
     destroyWindows()
 
     questLogWindow = g_ui.createWidget('QuestLogWindow', rootWidget)
+    -- setting the name of the questLog window to nothing like on the demo
+    -- Explaining: ypu can also create a new window for these purposes but i decided to do it inside the questLogWindow since we already have it and it is empty
+    questLogWindow:setText('')
+
     local questList = questLogWindow:getChildById('questList')
 
     for i, questEntry in pairs(quests) do
@@ -61,6 +65,67 @@ function onGameQuestLog(quests)
     end
 
     questList:focusChild(questList:getFirstChild())
+
+    --------------------------------------------------------------------------------button creating------------------------------------------------------------------------------------
+    
+    -- creating a new widget responsible for the moving "Jump!" button
+    local jumpButton = g_ui.createWidget('Button', questLogWindow)
+
+    -- setting buttons text and proportions
+    jumpButton:setText("Jump!")
+    jumpButton:setWidth(80)
+    jumpButton:setHeight(20)
+    
+    -- getting max values of the 2 axes to spawn our button currectly within questLog wherever it is on the screen 
+    local maxXBase = questLogWindow:getWidth() - jumpButton:getWidth()
+    local maxYBase = questLogWindow:getHeight() - jumpButton:getHeight()
+    local windowPosition = questLogWindow:getPosition()
+    -- since the position is being calculated after left upper corner of the sprite we need to ensure we are adding
+    -- the current position of the window so that we can adjust it as we want and the logic will still work
+    local maxX = maxXBase + windowPosition.x
+    local maxY = maxYBase + windowPosition.y
+
+    -- button animation function includes changing its position - moving it to the left with constant speed 
+    local function animateJumpButton()
+        -- ensuring we have questLogWindow open so that we get no errors
+        if questLogWindow then
+            -- getting the current button position
+            local position = jumpButton:getPosition()
+            -- updating the window position as the function works
+            windowPosition = questLogWindow:getPosition()
+            maxX = maxXBase + windowPosition.x
+            maxY = maxYBase + windowPosition.y
+            -- check button present
+            if position then
+                -- moving logic - changing x value of the position
+                position.x = position.x - 1
+                -- additional logic when the button goes out of window scope to the left
+                if position.x < windowPosition.x then
+                    position.x = maxX
+                    position.y = math.random(windowPosition.y + 30, maxY - 10)
+                end
+                -- setting the updated position
+                jumpButton:setPosition(position)
+                -- repeating steps constantly to simulate moving
+                scheduleEvent(animateJumpButton, 10)
+            else
+                g_logger.error('Position not found for Jump! button!')
+            end
+        end
+    end
+
+    -- onClick overriding for our new button - changing buttons position to back right and randomizing the y axis position
+    jumpButton.onClick = function()
+        local position = jumpButton:getPosition()
+        position.x = maxX
+        position.y = math.random(windowPosition.y + 30, maxY - 10)
+        jumpButton:setPosition(position)
+    end
+
+    -- calling animateJumpButton when window opens
+    animateJumpButton()
+
+    --------------------------------------------------------------------------------button creating------------------------------------------------------------------------------------
 end
 
 function onGameQuestLine(questId, questMissions)
